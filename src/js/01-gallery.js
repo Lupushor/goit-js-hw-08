81ca1b597777d5ce02fe0f2db3cb9222e52d94e8
Lupushor <alexandr.lupu.1984@gmail.com>
2023-03-14 16:01:20 +0100

    HW

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+const imagesContainer = document.querySelector('.gallery');
+
+const imgElemtens = galleryItems.reduce((acum, item) => {
+  return (acum += `<a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>`);
+}, '');
+
+imagesContainer.insertAdjacentHTML('beforeend', imgElemtens);
+
+new SimpleLightbox('.gallery a', {
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
