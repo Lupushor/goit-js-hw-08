a3f95de68611688450b788d6e540fd6f3076c144
Lupushor <alexandr.lupu.1984@gmail.com>
2023-03-16 11:09:18 +0100

    HW8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,10 @@
+import Player from '@vimeo/player';
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
+player.on('timeupdate', function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 });
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
